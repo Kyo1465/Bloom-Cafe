6c27deebaaf0db71f42d71267091eccfe019e036
Zhiger <ttemirkhanov.zhiger@gmail.com>
2026-06-09 15:26:23 +0500

    fixed header

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const menu = document.querySelector('.header__nav ul');
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 } );
+
+
+const anchors = document.querySelectorAll(".header__nav a");
+
+anchors.forEach(anc =>{
+  anc.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const id = anc.getAttribute("href");
+    
+    const elem = document.querySelector(id);
+
+    window.scroll({
+      top: elem.offsetTop - 80,
+      behavior: "smooth"
+    });
+  });
+})
